84f877ef5e109a697abff88d8d742431a959d579
Michael <mpermyashkin@gmail.com>
2020-07-07 00:55:06 -0400

    Complete filtering rework to pass params through request instead of distinct views

--- cloudland/store/urls.py
+++ cloudland/store/urls.py
@@ -5,12 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.products_page_all, name='store-products-page-all-home'),
     path('products/all/', views.products_page_all, name='store-products-page-all'),
-    path('products/categories/<slug:slug>/', views.products_by_category, name='store-products-page-filter-category'),
-    path('products/sellers/<slug:slug>/', views.products_by_seller, name='store-products-page-filter-seller'),
-    path('products/order_by=<str:order_by>', views.products_order_by, name='store-products-order-by'),
-    path('products/min_price=<int:min_price>&max_price=<int:max_price>/', views.products_by_price, name='store-products-page-filter-price'),
-    path('products/min_price=<int:min_price>/', views.products_by_price_min, name='store-products-page-filter-price'),
-    path('products/item/pruduct_id=<int:id>/', views.view_item, name='store-view-item'),
+    path('c/<slug:slug>/', views.products_by_category, name='store-products-page-filter-category'),
+    path('s/<slug:slug>/', views.products_by_seller, name='store-products-page-filter-seller'),
+    path('products/v/item=<int:id>/', views.view_item, name='store-view-item'),
     path('contact/', views.contact, name='store-contact'),
     path('about/', views.about, name='about'),
 ]
--- cloudland/store/views.py
+++ cloudland/store/views.py
@@ -12,12 +12,12 @@ from django.contrib.auth.decorators import login_required
 
 categories = Category.objects.order_by('name') # list of all categories 
 sellers = Seller.objects.all().filter(is_active=True).order_by('seller_listing_name') # list of all sellers 
-
+filterPath = ''
 # list of all currently active filters
 active_filters = {
     'category': 'category_All',
-    'seller': '',
-    'price': '',
+    'seller': 'seller_All',
+    'price': 'price_default',
     'min_price': '',
     'max_price': '',
     'order_by': '?',
@@ -28,8 +28,9 @@ active = [] # array of active filter id's passed into template
 def resetDefaultFilters():
     global active_filters
     active_filters = {
-        'seller': '',
+        'seller': 'seller_All',
         'category': 'category_All',
+        'price': 'price_default',
         'min_price': '',
         'max_price': '',
         'order_by': '?',
@@ -43,106 +44,152 @@ def getActiveList():
         if active_filters[key] != '':
             active.append(active_filters[key])
 
-# # Builds pagination system from product liust to display
-# def buildPaginator(request, products):
-#     paginator = Paginator(products, 12) # Show 20 products per page.
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     return page_obj
+# Builds string to display the active filters in template
+def buildFilterPath():
+    path = ''
+    category = active_filters['category']
+    if 'All' in category: 
+        category = 'All Categories'
+
+    seller = active_filters['seller']
+    if 'All' in seller: 
+        seller = 'All Sellers'
+    else:
+        seller_odj = sellers.get(slug=seller)
+        seller = seller_odj.seller_listing_name
+
+    price = active_filters['price']
+    if 'default' in price:
+        price = 'Default $'
+    else:
+        num_str = price.split("_",1)[1]
+        numbers = num_str.split('-')
+        numbers = [int(i) for i in numbers] 
+        if len(numbers) == 2:
+            price = '$'+str(min(numbers))+'.00-$'+str(max(numbers))+'.00'
+        if len(numbers) == 1:
+            price = '$'+str(numbers[0])+'+'
+
+    order = active_filters['order_by']
+    orders = {
+        '?': 'Order Default', 
+        '-date_listed': 'Latest',
+        'price': 'Low to High',
+        '-price': 'High to Low'
+    }
+    for key, value in orders.items(): 
+        if key == order:
+            order = value
+
+    global filterPath
+    filterPath = category.title() + ' / ' + seller.title() + ' / ' + price.title() + ' / ' + order.title()
 
 # Gets all products that match the current filters settings
 def getProductsWithActiveFilters():
     # products = Product.objects.filter(quantity__gte=1).order_by('?')
     products = Product.objects.filter(seller__is_active=True).order_by('?')
     getActiveList()
+    # Min and Max Price
     if active_filters['min_price'] != '' and active_filters['max_price'] != '':
         products = products.filter(price__gte=active_filters['min_price'], price__lte=active_filters['max_price'])
+    # Min Price
     if active_filters['min_price'] != '' and active_filters['max_price'] == '':
         products = products.filter(price__gte=active_filters['min_price'])
-    if active_filters['seller'] != '':
+    # Max Price
+    if active_filters['min_price'] == '' and active_filters['max_price'] != '':
+        products = products.filter(price__lte=active_filters['min_price'])
+    # Seller
+    if active_filters['seller'] != 'seller_All' and active_filters['seller'] != '':
         products = products.filter(seller__slug = active_filters['seller'])
+    # Category
     if active_filters['category'] != 'category_All' and active_filters['category'] != '':
         products = products.filter(category__slug = active_filters['category'])
+    # Order by
     if active_filters['order_by'] != '':
         products = products.order_by(active_filters['order_by'])
+    
+    buildFilterPath()
     return products
 
 
 # Products page to view all items
 def products_page_all(request):
     resetDefaultFilters()
+    getParamResolver(request)
     products = getProductsWithActiveFilters()
+
     args = {
         'products': products,
         'sellers': sellers,
         'categories': categories,
         'active': active,
+        'filterPath': filterPath
     }
     return render(request, 'store/products-page.html', args)
 
 # Filter products by selected seller
 def products_by_seller(request, slug):
-    active_filters['seller'] = slug
+    getParamResolver(request)
+    if slug == 'All':
+        active_filters['seller'] = 'seller_All'
+    else:
+        active_filters['seller'] = slug
     products = getProductsWithActiveFilters()
     args = {
         'products': products,
         'sellers': sellers,
         'categories': categories,
         'active': active,
+        'filterPath': filterPath
     }
     return render(request, 'store/products-page.html', args)
 
 # Filter products by selected category
 def products_by_category(request, slug):
-    active_filters['category'] = slug
+    getParamResolver(request)
+    if slug == 'All':
+        active_filters['category'] = 'category_All'
+    else:
+        active_filters['category'] = slug
     products = getProductsWithActiveFilters()
     args = {
         'products': products,
         'sellers': sellers,
         'categories': categories,
         'active': active,
-    }
-    return render(request, 'store/products-page.html', args)
-
-# Sets the order_by filter
-def products_order_by(request, order_by):
-    active_filters['order_by'] = order_by
-    products = getProductsWithActiveFilters()
-    args = {
-        'products': products,
-        'sellers': sellers,
-        'categories': categories,
-        'active': active,
+        'filterPath': filterPath
     }
     return render(request, 'store/products-page.html', args)
 
 
-# Filter products by price where lower and upper bound is defined
-def products_by_price(request, min_price, max_price):
-    active_filters['price'] = 'price_'+str(min_price)+'-'+str(max_price)
-    active_filters['min_price'] = min_price
-    active_filters['max_price'] = max_price
-    products = getProductsWithActiveFilters()
-    args = {
-        'products': products,
-        'sellers': sellers,
-        'categories': categories,
-        'active': active,
-    }
-    return render(request, 'store/products-page.html', args)
+def getParamResolver(request):
+    global active_filters
+    # Price GET params
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    price_default = request.GET.get('price')
+    if price_default:
+        active_filters['price'] = 'price_default'
+    else:
+        if min_price and not max_price:
+            if min_price.isnumeric():
+                active_filters['price'] = 'price_'+str(min_price)
+                active_filters['min_price'] = min_price
+        elif min_price and max_price:
+            if min_price.isnumeric() and max_price.isnumeric():
+                active_filters['price'] = 'price_'+str(min_price)+'-'+str(max_price)
+                active_filters['min_price'] = min_price
+                active_filters['max_price'] = max_price
+            else:
+                pass
 
-# Filter products by price where only lower bound is defined
-def products_by_price_min(request, min_price):
-    active_filters['price'] = 'price_'+str(min_price)
-    active_filters['min_price'] = min_price
-    products = getProductsWithActiveFilters()
-    args = {
-        'products': products,
-        'sellers': sellers,
-        'categories': categories,
-        'active': active,
-    }
-    return render(request, 'store/products-page.html', args)
+    # order_by GET param
+    valid_orders = ['?', '-date_listed', 'price', '-price']
+    order_by = request.GET.get('order_by')
+    if order_by in valid_orders:
+        active_filters['order_by'] = order_by
+
+    return
 
 # View when an item is selected to display item detail
 def view_item(request, id):
